e563d9f80400808390435f74f028c9afbce6166b
Jonas Berglund <berglund.jonas@gmail.com>
2015-08-12 00:15:07 +0200

    remove navbar at onboarding

--- client/views/onboarding/onboarding.js
+++ client/views/onboarding/onboarding.js
@@ -1,7 +1,11 @@
 Template.onboarding.rendered = function() {
   IonSideMenu.snapper.disable();
+  $('.bar-header').hide();
+  $('.content').removeClass('has-header');
 };
 
 Template.onboarding.destroyed = function() {
   IonSideMenu.snapper.enable();
+  $('.bar-header').show();
+  //$('.content').addClass('has-header');
 };

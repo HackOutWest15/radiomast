7b05e0ec871abc77b2427fdc2718a1aad41889a7
Jonas Berglund <berglund.jonas@gmail.com>
2015-08-11 16:51:19 +0200

    add search bar

--- client/views/partials/player/player.js
+++ client/views/partials/player/player.js
@@ -1,13 +1,42 @@
 Template.player.events({
+  'click [data-search]': function () {
+      console.log('Search');
+
+        var query = $('input[name="song-query"]').val();
+        if(query.length == 0) return;
+
+        var query = $('input').val();
+
+        // spotify.search(query, 'tracks', function(error, result) {
+        //   if(error){
+        //     return console.log(error);
+        //   }
+        //   console.log(result);
+        // });
+
+
+        Meteor.call('getSearch', query, function(err, result){
+          if(err){
+            return console.log(err);
+          }
+
+          $('input[name="song-uri"]').val(result);
+        });
+
+        // Session.set('currentSong', uri);
+        //
+        // SongPlayer.play(uri, function(err){
+        //   console.log(err);
+        //   console.log('1336 1336');
+        // });
+   },
   'click [data-play]': function () {
       console.log('@#!@#!@#');
 
-        var uri = $('input').val();
+        var uri = $('input[name="song-uri"]').val();
 
         if(uri.length == 0) return;
 
-
-        var uri = $('input').val();
         Session.set('currentSong', uri);
 
         SongPlayer.play(uri, function(err){
@@ -21,5 +50,3 @@ Template.player.events({
       Session.set('crossfade', sliderValue.toString());
    }
 });
-
-
--- server/spotifyNativeAPI.js
+++ server/spotifyNativeAPI.js
@@ -7,8 +7,18 @@ Meteor.methods({
       accessToken: user.services.spotify.accessToken,
       expirationDate: (user.services.spotify.expiresAt).toString()
     }
+  },
+  getSearch: function(query) {
+    console.log(query);
+    var result = HTTP.get("https://api.spotify.com/v1/search?q=" + query + "&type=track", {
+         headers: {
+             Accept: "application/json"
+         }
+    });
+    console.log(result.data.tracks.items[0].uri);
+    return result.data.tracks.items[0].uri;
+
+    //console.log(result.content);
+    //console.log(result.data);
   }
 });
-
-
-

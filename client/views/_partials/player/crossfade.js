c546e1e978f8f4b1faabaa1d52eff85777657555
malin.anker <anker@student.chalmers.se>
2015-08-12 09:26:18 +0200

    css changes

--- client/views/_partials/player/crossfade.js
+++ client/views/_partials/player/crossfade.js
@@ -1,10 +1,19 @@
+var fadeTimeout = null;
+
 Template.crossfade.events({
    'input [data-crossfade]': function (event) {
       var sliderValue = parseInt(event.currentTarget.value) / 100;
 
       Session.set('crossfade', sliderValue.toString());
       Meteor.call('changeVolume', sliderValue.toString());
-   }
+
+			IonSideMenu.snapper.disable();
+   },
+    'touchend': function (event) {
+			fadeTimeout = setTimeout(function(){
+				IonSideMenu.snapper.enable();
+			}, 500);
+		}
 });
 
 
--- client/views/_partials/sideMenus/sideMenus.js
+++ client/views/_partials/sideMenus/sideMenus.js
@@ -1,9 +1,18 @@
+Meteor.startup(function(){
+
+	setTimeout(function(){
+		IonSideMenu.snapper.settings({
+      		maxPosition: "390",
+      		minPosition: "-390"
+    	}); 	
+	}, 100);
+});
+
 Template.leftSideMenu.events({
   'click a': function () {
     IonSideMenu.snapper.close();
   }
 });
-
 
 Template.rightSideMenu.events({
   'click a': function () {
